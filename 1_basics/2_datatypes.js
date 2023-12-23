cc53580ecac43ba8a24d21738b763f5dd303b7ec
D ANIL KUMAR <98248440+DAnilAk1109@users.noreply.github.com>
2023-12-23 21:57:41 +0530

    Update 2_datatypes.js

--- 1_basics/2_datatypes.js
+++ 1_basics/2_datatypes.js
@@ -1,8 +1,39 @@
-/*  number
+/*  
+    primitive data types :
+    number
     bigint      
     string      ""
     boolean     true or false
     null        standalone value but it is an object
     undefined
     symbol      unique
+*/
+/*
+    non primitive
+    array
+    object
+
+*/
+
+// examples
+/*
+
+Numbers:
+let length = 16;
+let weight = 7.5;
+
+Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+Booleans
+let x = true;
+let y = false;
+
+Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
 */
